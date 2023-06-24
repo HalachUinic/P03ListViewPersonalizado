661f1e1c930a536502ecd72ff255bf814879e367
HalachUinic <134268219+HalachUinic@users.noreply.github.com>
2023-06-23 23:39:35 -0700

    Layout MainActivity

--- app/src/main/java/com/example/p03listviewpersonalizado/MainActivity.java
+++ app/src/main/java/com/example/p03listviewpersonalizado/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.p03listviewpersonalizado;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,61 +10,70 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
-
+import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private SearchView srcLista;
     private AdapterAlumno adapter;
-    private ListView sp;
+    private ListView listView;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        srcLista = findViewById(R.id.menu_search);
 
-        ArrayList<AlumnoItem> list = new ArrayList<>();
-        list.add(new AlumnoItem(getString(R.string.itemEstefano), getString(R.string.Estefano), R.drawable.ft2019030344));
-        list.add(new AlumnoItem(getString(R.string.itemCarlosAlberto), getString(R.string.CarlosAlberto), R.drawable.ft2020030174));
-        list.add(new AlumnoItem(getString(R.string.itemMarcoAntonio), getString(R.string.MarcoAntonioAlarid), R.drawable.ft2020030176));
-        list.add(new AlumnoItem(getString(R.string.itemJoshuaDaniel), getString(R.string.JoshuaDaniel), R.drawable.ft2020030181));
-        list.add(new AlumnoItem(getString(R.string.itemErnestoDavid), getString(R.string.ErnestoDavid), R.drawable.ft2020030184));
-        list.add(new AlumnoItem(getString(R.string.itemMaximiliano), getString(R.string.Maximiliano), R.drawable.ft2020030189));
-        list.add(new AlumnoItem(getString(R.string.itemIvanEnrique), getString(R.string.IvanEnrique), R.drawable.ft2020030199));
-        list.add(new AlumnoItem(getString(R.string.itemJesusEduardo), getString(R.string.JesusEduardo), R.drawable.ft2020030212));
-        list.add(new AlumnoItem(getString(R.string.itemDavidAntonio), getString(R.string.DavidAntonio), R.drawable.ft2020030241));
-        list.add(new AlumnoItem(getString(R.string.itemGustavoAdolfo), getString(R.string.GustavoAdolfo), R.drawable.ft2020030243));
-        list.add(new AlumnoItem(getString(R.string.itemJuanCarlos), getString(R.string.JuanCarlos), R.drawable.ft2020030249));
-        list.add(new AlumnoItem(getString(R.string.itemAlejo), getString(R.string.Alejo), R.drawable.ft2020030264));
-        list.add(new AlumnoItem(getString(R.string.itemCarlosJair), getString(R.string.CarlosJair), R.drawable.ft2020030268));
-        list.add(new AlumnoItem(getString(R.string.itemJesusTadeo), getString(R.string.JesusTadeo), R.drawable.ft2020030292));
-        list.add(new AlumnoItem(getString(R.string.itemJair), getString(R.string.Jair), R.drawable.ft2020030304));
-        list.add(new AlumnoItem(getString(R.string.itemDiego), getString(R.string.Diego), R.drawable.ft2020030306));
-        list.add(new AlumnoItem(getString(R.string.itemSalmaYareth),getString(R.string.SalmaYareth),R.drawable.ft2020030315));
-        list.add(new AlumnoItem(getString(R.string.itemJesusAntonio), getString(R.string.JesusAntonio), R.drawable.ft2020030333));
-        list.add(new AlumnoItem(getString(R.string.itemYenniferCarolina), getString(R.string.YenniferCarolina), R.drawable.ft2020030389));
-        list.add(new AlumnoItem(getString(R.string.itemManuelAlexis), getString(R.string.ManuelAlexis), R.drawable.ft2020030766));
-        list.add(new AlumnoItem(getString(R.string.itemVictorMoises), getString(R.string.VictorMoises), R.drawable.ft2020030771));
-        list.add(new AlumnoItem(getString(R.string.itemHalachUinic), getString(R.string.HalachUinic), R.drawable.ft2020030777));
-        list.add(new AlumnoItem(getString(R.string.itemEnzoAlejandro), getString(R.string.EnzoAlejandro), R.drawable.ft2020030799));
-        list.add(new AlumnoItem(getString(R.string.itemJoseAlberto), getString(R.string.JoseAlberto), R.drawable.ft2020030808));
-        list.add(new AlumnoItem(getString(R.string.itemGiancarlo), getString(R.string.Giancarlo), R.drawable.ft2020030819));
-        list.add(new AlumnoItem(getString(R.string.itemLuisAngel), getString(R.string.LuisAngel), R.drawable.ft2020030865));
-        //Sí los puse a mano, se que pude haber utilizado una herramienta *guiño guiño* pero se me fue el rollo :c
-        sp = findViewById(R.id.listView);
+
+        ArrayList<AlumnoItem> list = new ArrayList<>(Arrays.asList(
+                new AlumnoItem(getString(R.string.itemEstefano), getString(R.string.Estefano), R.drawable.ft2019030344),
+                new AlumnoItem(getString(R.string.itemCarlosAlberto), getString(R.string.CarlosAlberto), R.drawable.ft2020030174),
+                new AlumnoItem(getString(R.string.itemMarcoAntonio), getString(R.string.MarcoAntonioAlarid), R.drawable.ft2020030176),
+                new AlumnoItem(getString(R.string.itemJoshuaDaniel), getString(R.string.JoshuaDaniel), R.drawable.ft2020030181),
+                new AlumnoItem(getString(R.string.itemErnestoDavid), getString(R.string.ErnestoDavid), R.drawable.ft2020030184),
+                new AlumnoItem(getString(R.string.itemMaximiliano), getString(R.string.Maximiliano), R.drawable.ft2020030189),
+                new AlumnoItem(getString(R.string.itemIvanEnrique), getString(R.string.IvanEnrique), R.drawable.ft2020030199),
+                new AlumnoItem(getString(R.string.itemJesusEduardo), getString(R.string.JesusEduardo), R.drawable.ft2020030212),
+                new AlumnoItem(getString(R.string.itemDavidAntonio), getString(R.string.DavidAntonio), R.drawable.ft2020030241),
+                new AlumnoItem(getString(R.string.itemGustavoAdolfo), getString(R.string.GustavoAdolfo), R.drawable.ft2020030243),
+                new AlumnoItem(getString(R.string.itemJuanCarlos), getString(R.string.JuanCarlos), R.drawable.ft2020030249),
+                new AlumnoItem(getString(R.string.itemAlejo), getString(R.string.Alejo), R.drawable.ft2020030264),
+                new AlumnoItem(getString(R.string.itemCarlosJair), getString(R.string.CarlosJair), R.drawable.ft2020030268),
+                new AlumnoItem(getString(R.string.itemJesusTadeo), getString(R.string.JesusTadeo), R.drawable.ft2020030292),
+                new AlumnoItem(getString(R.string.itemJair), getString(R.string.Jair), R.drawable.ft2020030304),
+                new AlumnoItem(getString(R.string.itemAngelRonaldoAnastacio), getString(R.string.AngelRonaldoAnastacio), R.drawable.ft2020030329),
+                new AlumnoItem(getString(R.string.itemAngelZinedineAnastacio), getString(R.string.AngelZinedineAnastacio), R.drawable.ft2020030325),
+                new AlumnoItem(getString(R.string.itemDiego), getString(R.string.Diego), R.drawable.ft2020030306),
+                new AlumnoItem(getString(R.string.itemSalmaYareth), getString(R.string.SalmaYareth), R.drawable.ft2020030315),
+                new AlumnoItem(getString(R.string.itemJesusAntonio), getString(R.string.JesusAntonio), R.drawable.ft2020030333),
+                new AlumnoItem(getString(R.string.itemYenniferCarolina), getString(R.string.YenniferCarolina), R.drawable.ft2020030389),
+                new AlumnoItem(getString(R.string.itemManuelAlexis), getString(R.string.ManuelAlexis), R.drawable.ft2020030766),
+                new AlumnoItem(getString(R.string.itemVictorMoises), getString(R.string.VictorMoises), R.drawable.ft2020030771),
+                new AlumnoItem(getString(R.string.itemHalachUinic), getString(R.string.HalachUinic), R.drawable.ft2020030777),
+                new AlumnoItem(getString(R.string.itemEnzoAlejandro), getString(R.string.EnzoAlejandro), R.drawable.ft2020030799),
+                new AlumnoItem(getString(R.string.itemJoseAlberto), getString(R.string.JoseAlberto), R.drawable.ft2020030808),
+                new AlumnoItem(getString(R.string.itemGiancarlo), getString(R.string.Giancarlo), R.drawable.ft2020030819),
+                new AlumnoItem(getString(R.string.itemLuisAngel), getString(R.string.LuisAngel), R.drawable.ft2020030865),
+                new AlumnoItem( getString(R.string.itemPaolaEmiret), getString(R.string.PaolaEmiret), R.drawable.ft2020030313)
+        ));
+
+
+        listView = findViewById(R.id.listView);
         adapter = new AdapterAlumno(this, R.layout.item_alumno, R.id.lblNombe, list);
-        sp.setAdapter(adapter);
-
-        sp.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(adapter.getContext(), "Se ha seleccionado".toString()+ " "+((AlumnoItem) adapterView.getItemAtPosition(i)).getTxtNombre(),Toast.LENGTH_SHORT).show();
-            }
-        });
+                Toast.makeText(MainActivity.this, "Se ha seleccionado " + ((AlumnoItem) adapterView.getItemAtPosition(i)).getTxtNombre(), Toast.LENGTH_SHORT).show();
+
+           }
+        }
+        );
+
     }
 
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.searchview, menu);
@@ -86,4 +94,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onCreateOptionsMenu(menu);
     }
+
 }
